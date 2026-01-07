f365f859b545cf3899d09ff13de488c2914ef313
mumfy99 <tace.mumford@hotmail.com>
2026-01-07 13:18:28 +0100

    Update Influx scripts and scene

--- Assets/Scripts/InfluxAlarm.cs
+++ Assets/Scripts/InfluxAlarm.cs
@@ -9,9 +9,9 @@ using TMPro;
 public class InfluxAlarm : MonoBehaviour
 {
     [Header("InfluxDB Settings")]
-    public string influxUrl = "http://130.130.130.209:8086";
-    public string orgId = "66751340bcb17c31";
-    public string apiToken = "PASTE_YOUR_TOKEN_HERE";
+    public string influxUrl = "http://130:130:130:205:1880";
+    public string orgId = "8d72c1fb56ba4914";
+    public string apiToken = "CVU6Py8WeUXfkQcdpXXSFJg2MvvbV5PiPEfK2KEbK0HSssgG9QN4YbSVUl-Mo7T4_7wJGX6CL_ZB-_UAQI4Dmg==";
 
     [Header("Query Settings")]
     // SIF 401 → "Sif 401"
--- Assets/Scripts/influxAlarmData.cs
+++ Assets/Scripts/influxAlarmData.cs
@@ -14,9 +14,9 @@ using System.Text.RegularExpressions;
 public class InfluxAlarmDataProvider : MonoBehaviour
 {
     [Header("InfluxDB Settings")]
-    public string influxUrl = "http://130.130.130.208:8086";
-    public string orgId = "66751340bcb17c31";
-    public string apiToken = "rxFXXk69LjakTyXY4TLCKbncEe6LNIBCqQ46SuUBVwV-CvLw3M6NsoI5h7TcbpKHn1FLd344ZiBrmv3pPFL1Rg==";
+    public string influxUrl = "http://130.130.130.205:1880";
+    public string orgId = "8d72c1fb56ba4914";
+    public string apiToken = "CVU6Py8WeUXfkQcdpXXSFJg2MvvbV5PiPEfK2KEbK0HSssgG9QN4YbSVUl-Mo7T4_7wJGX6CL_ZB-_UAQI4Dmg==";
 
 
     [Header("Query Settings")]
